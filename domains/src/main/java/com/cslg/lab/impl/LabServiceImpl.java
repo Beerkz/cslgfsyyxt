4f7c9c90358ce950bd3a27ce8f7c4883fdef91cb
Kay.Zhang <kay.zhang@safesoftcorp.com>
2023-03-01 13:05:05 +0800

    实验室分时预约系统

--- domains/src/main/java/com/cslg/lab/impl/LabServiceImpl.java
+++ domains/src/main/java/com/cslg/lab/impl/LabServiceImpl.java
@@ -1,6 +1,9 @@
 package com.cslg.lab.impl;
 
 import com.cslg.lab.LabService;
+import com.cslg.lab.param.PageLabCondition;
+import com.cslg.lab.vo.PageLabVo;
+import com.cslg.vo.JsonPagedVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LabServiceImpl implements LabService {
 
+    @Override
+    public JsonPagedVO<PageLabVo> pageLab(PageLabCondition pageLabCondition) {
+        return null;
+    }
 }
